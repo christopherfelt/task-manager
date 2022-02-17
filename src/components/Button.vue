b0a8cd0a6d2c598d556d5014dbbff72b5fd6cec1
christopherfelt <chris.h.felt@gmail.com>
2022-02-17 15:23:08 -0700

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button :style="{background: color}"  class="btn" >{{title}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button-Component',
+    props:{
+        title: String,
+        color: String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+      <h1>{{title}}</h1>
+      <Button title="Add Task" color="green" />
+      <Button title="Update Task" color="blue" />
+  </header>
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+    name: 'Header-Component',
+    props: {
+        title: {
+            type: String,
+            default: "this is the default"
+        }
+    },
+    components:{
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
